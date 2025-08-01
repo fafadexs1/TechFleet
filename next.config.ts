5b0f925243d0d5292c153ad9bad92aa6eca0bc53
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-01 15:36:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Re-added to force recompile
   typescript: {
     ignoreBuildErrors: true,
   },
