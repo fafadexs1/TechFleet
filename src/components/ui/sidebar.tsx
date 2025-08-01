616524ebaf6a02466081e6e01036618ae20b164f
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-01 19:34:20 +0000

    O menu está sobrepondo a página sendo que os dois deveriam estar ajustad

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -289,8 +289,8 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
-        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+        "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-300 ease-in-out md:ml-[var(--sidebar-width)] peer-data-[state=collapsed]:md:ml-[var(--sidebar-width-icon)]",
+        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 peer-data-[state=expanded]:md:peer-data-[variant=inset]:ml-[var(--sidebar-width)] peer-data-[state=collapsed]:md:peer-data-[variant=inset]:ml-[var(--sidebar-width-icon)] md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
       {...props}
